b3ff69866d603c114c8bd8ff29c2d4f5b512f3da
DemetreShonia <shonka.demeloper@gmail.com>
2023-06-18 12:54:16 +0400

    Map Page Non-Responsive

--- Project/src/components/Map.jsx
+++ Project/src/components/Map.jsx
@@ -1,10 +1,36 @@
 import "../Styles/Map.css";
 import Samegrelo from "../Images/Samegrelo.png";
 import Star from "../Images/star.png";
+import red from "../Images/location-pin-red.png";
+import green from "../Images/location-green.png";
+import orange from "../Images/location-orange.png";
+import present from "../Images/giftbox.png";
 
 const Map = () => {
+  const onGreenPinClicked = () => {
+    console.log("GREEN");
+  };
+  const onRedPinClicked = () => {
+    console.log("RED");
+  };
+  const onOrangePinClicked = () => {
+    console.log("Orange");
+  };
   return (
     <div className="map">
+      <img
+        src={green}
+        alt=""
+        className="green-pin"
+        onClick={onGreenPinClicked}
+      />
+      <img src={red} alt="" className="red-pin" onClick={onRedPinClicked} />
+      <img
+        src={orange}
+        alt=""
+        className="orange-pin"
+        onClick={onOrangePinClicked}
+      />
       <img src={Samegrelo} alt="" />
       <div className="bottom">
         <div class="level-up-bar">
@@ -46,6 +72,13 @@ const Map = () => {
                   <img className="star" src={Star} alt="" />
                 </div>
               </div>
+            </div>
+          </div>
+
+          <div className="prizes">
+            შეთავაზებები
+            <div className="prize-img">
+              <img className="prize" src={present} alt="" />
             </div>
           </div>
         </div>
