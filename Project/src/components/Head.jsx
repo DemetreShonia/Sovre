cd09596c70ea0f3ccd09ed64e685e0dab2ba0c79
Mariam <109237745+MariamSh03@users.noreply.github.com>
2023-06-18 14:00:15 +0400

    updated

--- Project/src/components/Head.jsx
+++ Project/src/components/Head.jsx
@@ -1,15 +1,15 @@
 import "../Styles/Head.css"
 import logo from "../icons/sovre-logo.png"
-import guide from "../icons/tour.png"
+
 
 const Head = () => {
     return ( <div className="head"> 
-    <img src={guide} className="guide"/>
     <img src={logo} className="logo" />
+    <div className="user"> Guest </div>
     <div className="switch">
       <input id="language-toggle" className="check-toggle check-toggle-round-flat" type="checkbox" />
       <label htmlFor="language-toggle"></label>
-      <span className="on">GEO</span>
+      <span className="on">ქარ</span>
       <span className="off">ENG</span>
     </div>
   </div>
