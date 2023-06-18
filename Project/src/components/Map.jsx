ced714ebf63c3ccec9988d27207d1b2ac6eff433
DemetreShonia <shonka.demeloper@gmail.com>
2023-06-18 14:17:03 +0400

    Suggestions Mostly Done

--- Project/src/components/Map.jsx
+++ Project/src/components/Map.jsx
@@ -5,8 +5,11 @@ import red from "../Images/location-pin-red.png";
 import green from "../Images/location-green.png";
 import orange from "../Images/location-orange.png";
 import present from "../Images/giftbox.png";
+import { useState } from "react";
+import goldenFlee from "../Images/golden-flee.jfif";
 
 const Map = () => {
+  const [isSuggOn, setIsSuggOn] = useState(false);
   const onGreenPinClicked = () => {
     console.log("GREEN");
   };
@@ -16,6 +19,95 @@ const Map = () => {
   const onOrangePinClicked = () => {
     console.log("Orange");
   };
+  const onClickSuggestions = () => {
+    setIsSuggOn(true);
+    console.log();
+  };
+  const onClickDisableSuggestions = () => {
+    setIsSuggOn(false);
+  };
+
+  if (isSuggOn)
+    return (
+      <div className="suggestions">
+        <div className="btn-bar">
+          <div className="close-btn" onClick={onClickDisableSuggestions}>
+            X
+          </div>
+        </div>
+        <div className="content">
+          <h2 className="sale">ფასდაკლება</h2>
+          <hr />
+          <div className="element">
+            <img className="partner-img" src={goldenFlee} alt="" />
+            <div className="img-holder">
+              <div className="price">3000</div>
+              <img className="star-sm" src={Star} alt="" />
+            </div>
+            <div className="other-cont">
+              <div className="discount">
+                <h4 className="claim-txt">-40%</h4>
+              </div>
+              <div className="claim-btn">
+                <div className="prize-img">
+                  <img className="prize-sm" src={present} alt="" />
+                </div>
+                <h4 className="claim-txt">მიღება</h4>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="element">
+            <img className="partner-img" src={goldenFlee} alt="" />
+            <div className="img-holder">
+              <div className="price">3000</div>
+              <img className="star-sm" src={Star} alt="" />
+            </div>
+            <div className="other-cont">
+              <div className="discount">
+                <h4 className="claim-txt">-40%</h4>
+              </div>
+              <div className="claim-btn">
+                <div className="prize-img">
+                  <img className="prize-sm" src={present} alt="" />
+                </div>
+                <h4 className="claim-txt">მიღება</h4>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="element">
+            <img className="partner-img" src={goldenFlee} alt="" />
+            <div className="img-holder">
+              <div className="price">3000</div>
+              <img className="star-sm" src={Star} alt="" />
+            </div>
+            <div className="other-cont">
+              <div className="discount">
+                <h4 className="claim-txt">-40%</h4>
+              </div>
+              <div className="claim-btn">
+                <div className="prize-img">
+                  <img className="prize-sm" src={present} alt="" />
+                </div>
+                <h4 className="claim-txt">მიღება</h4>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="level-progress-sale">
+            <div className="level-bar-bg">
+              <div className="level-bar">UserID: Guest1057</div>
+              <div className="level-bar-empty">1300</div>
+              <div className="img-holder">
+                <img className="star-sm" src={Star} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
   return (
     <div className="map">
       <img
@@ -75,7 +167,7 @@ const Map = () => {
             </div>
           </div>
 
-          <div className="prizes">
+          <div className="prizes" onClick={onClickSuggestions}>
             შეთავაზებები
             <div className="prize-img">
               <img className="prize" src={present} alt="" />
