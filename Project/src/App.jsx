a4d91f3d2125f2c8ad5ddf4d39472401151d372c
DemetreShonia <shonka.demeloper@gmail.com>
2023-06-18 16:02:07 +0400

    Merged and Pages work not css

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/card";
 import Head from "./components/Head";
 import Map from "./Components/Map";
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
+
+  const openMap = () => setIsOn(true);
   return (
     <>
       <Head />
-      <Map />
+      {isOn ? <Map /> : <Card openMap={openMap} />}
     </>
   );
 }
--- Project/src/components/Card.jsx
+++ Project/src/components/Card.jsx
@@ -1,29 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import '../Styles/Card.css';
-import guide from '../icons/tour.png';
-import audio from '../icons/dadianiAudio.mp3';
+import React, { useState, useEffect } from "react";
+import "../Styles/Card.css";
+import guide from "../icons/tour.png";
+import audio from "../icons/dadianiAudio.mp3";
 
-import checked from "../icons/checked.png"
-import checkedDone from "../icons/checkedDone.png"
+import checked from "../icons/checked.png";
+import checkedDone from "../icons/checkedDone.png";
 
-const Card = () => {
+const Card = (props) => {
   const [showMore, setShowMore] = useState(false);
-  const [isDone , setIsDone] = useState(false);
+  const [isDone, setIsDone] = useState(false);
 
-
-
-
- 
   return (
     <div className="card">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Dadiani_Palazzio.png" className="img" />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Dadiani_Palazzio.png"
+        className="img"
+      />
       <div className="info">
-        დადიანების სასახლე - სამეგრელოს მთავრების, დადიანების რეზიდენცია იყო. ის ოდიშის მთავარმა ლევან II დადიანმა ააშენა,მე-17 საუკუნეში.ქალაქ ზუგდიდის ცენტრში მდებარეობს.
+        დადიანების სასახლე - სამეგრელოს მთავრების, დადიანების რეზიდენცია იყო. ის
+        ოდიშის მთავარმა ლევან II დადიანმა ააშენა,მე-17 საუკუნეში.ქალაქ ზუგდიდის
+        ცენტრში მდებარეობს.
         {showMore && (
           <div className="more" id="moreDiv">
-            მუზეუმში გამოფენილია ამ მხარის არქეოლოგიური (პალეოლითის, ნეოლითისა და ბრინჯაოს ხანის ნივთები) და ეთნოგრაფიული (სასოფლო-სამეურნეო იარაღი, საყოფაცხოვრებო ნივთები, ტანსაცმელი, საცხოვრებელ ნაგებობათა – „ფაცხა“, „ჯარგვალი“, „ოდა“ – და სხვა ნიმუშები) მასალა. წარმოდგენილია ფეოდალური ხანის საქართველოს ისტორიის ამსახველი ექსპონატები. სამეგრელოს უკანასკნელი მთავრის დავით დადიანისა და მისი ოჯახის წევრთა პირადი ნივთები.
-
-            ზუგდიდის დადიანების სასახლეთა ისტორიულ-არქიტექტურულ მუზეუმში ინახება მსოფლიო მნიშვნელობის სამუზეუმო ექსპონატები და რელიგიური სიწმინდეები. მუზეუმის გარშემო მდებარეობს სამეგრელოს მთავარ დადიანთა მიერ გაშენებული ტყე-პარკი 67 ჰექტარზე, სადაც ხარობს მსოფლიოს სხვადასხვა კუთხიდან ჩამოტანილი უნიკალური და მრავალწლიანი ხეები და ბუჩქები.
+            მუზეუმში გამოფენილია ამ მხარის არქეოლოგიური (პალეოლითის, ნეოლითისა
+            და ბრინჯაოს ხანის ნივთები) და ეთნოგრაფიული (სასოფლო-სამეურნეო
+            იარაღი, საყოფაცხოვრებო ნივთები, ტანსაცმელი, საცხოვრებელ ნაგებობათა –
+            „ფაცხა“, „ჯარგვალი“, „ოდა“ – და სხვა ნიმუშები) მასალა. წარმოდგენილია
+            ფეოდალური ხანის საქართველოს ისტორიის ამსახველი ექსპონატები.
+            სამეგრელოს უკანასკნელი მთავრის დავით დადიანისა და მისი ოჯახის წევრთა
+            პირადი ნივთები. ზუგდიდის დადიანების სასახლეთა ისტორიულ-არქიტექტურულ
+            მუზეუმში ინახება მსოფლიო მნიშვნელობის სამუზეუმო ექსპონატები და
+            რელიგიური სიწმინდეები. მუზეუმის გარშემო მდებარეობს სამეგრელოს მთავარ
+            დადიანთა მიერ გაშენებული ტყე-პარკი 67 ჰექტარზე, სადაც ხარობს
+            მსოფლიოს სხვადასხვა კუთხიდან ჩამოტანილი უნიკალური და მრავალწლიანი
+            ხეები და ბუჩქები.
           </div>
         )}
         {!showMore && (
@@ -41,11 +51,20 @@ const Card = () => {
         </audio>
       </div>
       <div className="buttons">
-        <div className="explore" >
+        <div className="explore" onClick={props.openMap}>
           აღმოაჩინე მეტი
         </div>
         <div className="checked">
-            <img className='checkmark' onClick={()=> { if(!isDone){setIsDone(true)}}} src={(!isDone)? checked : checkedDone} alt="" />
+          <img
+            className="checkmark"
+            onClick={() => {
+              if (!isDone) {
+                setIsDone(true);
+              }
+            }}
+            src={!isDone ? checked : checkedDone}
+            alt=""
+          />
         </div>
       </div>
     </div>
